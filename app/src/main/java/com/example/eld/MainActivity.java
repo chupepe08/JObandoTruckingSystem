64654b72c3d12a2fd7cba0449e1656316c195b8a
Jasel <patriajasel@gmail.com>
2022-05-30 09:49:22 +0800

    not final

--- app/src/main/java/com/example/eld/MainActivity.java
+++ app/src/main/java/com/example/eld/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,26 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 TextView passW = findViewById(R.id.pass);
                 String pass = passW.getText().toString();
 
-                if(user.equals("") || pass.equals("")){
-                    Toast.makeText(MainActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Boolean checkUserPass = database.checkUsernamePassword(user, pass);
-                    if(checkUserPass == true){
-                        Intent intent = new Intent(MainActivity.this, dashboard.class);
-                        startActivity(intent);
-                    }
-                    else{
-                        Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                /*if(user.equals("root")){
+                if(user.equals("root")){
                     if(pass.equals("admin")){
                         TextView invalid = findViewById(R.id.invalid);
                         invalid.setText("Login Successfully");
                         invalid.setVisibility(View.VISIBLE);
 
+                        Intent newAct = new Intent(MainActivity.this, dashboard.class);
+                        startActivity(newAct);
 
                     }else{
                         TextView invalid = findViewById(R.id.invalid);
@@ -65,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
                     userN.setText("");
                     passW.setText("");
-                }*/
+                }
             }
         });
     }
